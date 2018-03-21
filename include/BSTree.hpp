476fa8176768b1ed0051d98445e8b1884021a3a3
Volkov17 <32384533+Volkov17@users.noreply.github.com>
2018-03-21 15:13:40 +0300

    Update BSTree.hpp

--- include/BSTree.hpp
+++ include/BSTree.hpp
@@ -1,1 +1,22 @@
 
+#include<iostream>
+namespace BSTree {
+    struct Node {
+        int data;
+        Node *left;
+        Node *right;
+//        Tree *p;
+    };
+
+    class Tree {
+        Node *root;
+        void printEl(Node* node, int a);
+        void destroyTree(Node*&node) ;
+    public:
+        Tree();
+        bool insert(int value);
+        void print() ;
+        ~Tree();
+    };
+}
+
